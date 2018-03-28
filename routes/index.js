a22573f1e3fe374c06a2e4ba58a391a3c1fcdbb7
Markus Andersson <make_99@hotmail.com>
2018-03-28 13:34:33 +0200

    added rendering dynamicly of footer content

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
+//Taking in all our JSON
 let featuresJson = require('../json/features.json');
 let headingsJson = require('../json/headings.json');
+let footerJson = require('../json/footer.json');
 
 
 exports.home = function(req, res) {
+	//making sure Json is avalible for rendering
 	let features = featuresJson.features;
 	let headings = headingsJson.headings;
+	let footer = footerJson.footer;
 
 	res.render('home', {
 		title: "SenseSec",
 		features: features,
-		headings: headings
+		headings: headings,
+		footer: footer
 	});
 };
