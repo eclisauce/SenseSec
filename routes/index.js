0017792cd02611535e2d246cae3a727596d77607
Markus Andersson <make_99@hotmail.com>
2018-03-28 13:22:12 +0200

    now rending features dynamiclly from JSON and the headings on the site

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
+let featuresJson = require('../json/features.json');
+let headingsJson = require('../json/headings.json');
+
+
 exports.home = function(req, res) {
+	let features = featuresJson.features;
+	let headings = headingsJson.headings;
 
 	res.render('home', {
-		title: "SenseSec"
+		title: "SenseSec",
+		features: features,
+		headings: headings
 	});
 };
