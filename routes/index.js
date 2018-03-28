c05f4bd9edc8d5d1f11c7ec63f64c1597b5d154c
Markus Andersson <make_99@hotmail.com>
2018-03-28 14:43:58 +0200

    Small tinkerings, and new logo in footer and favicon

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-//Taking in all our JSON
+//Taking in all our JSON and rendering depending on section of the site
 let featuresJson = require('../json/features.json');
 let headingsJson = require('../json/headings.json');
 let footerJson = require('../json/footer.json');
 let ourworkJson = require('../json/ourwork.json');
+let siteInformationJson = require('../json/siteInformation.json');
 
 
 exports.home = function(req, res) {
@@ -11,12 +12,14 @@ exports.home = function(req, res) {
 	let headings = headingsJson.headings;
 	let footer = footerJson.footer;
 	let ourwork = ourworkJson.ourwork;
+	let siteInformation = siteInformationJson.siteInformation;
 
 	res.render('home', {
 		title: "SenseSec",
 		features: features,
 		headings: headings,
 		footer: footer,
-		ourwork:ourwork
+		ourwork:ourwork,
+		siteInformation: siteInformation
 	});
 };
